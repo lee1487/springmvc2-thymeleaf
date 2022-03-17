5c8b8de8ba6e0b8af5028c696a34150681e86fc5
이현세 <dlgustp1487@naver.com>
2022-03-17 13:23:49 +0900

    체크 박스 - 단일1

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,6 +3,8 @@ package hello.itemservice.domain.item;
 import java.util.List;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class Item {
@@ -12,7 +14,7 @@ public class Item {
     private Integer price;
     private Integer quantity;
     
-    private boolean open; 			//판매 여부 
+    private Boolean open; 			//판매 여부 
     private List<String> regions; 	//등록 지역 
     private ItemType itemType; 		//상품 종류
     private String deliveryCode; 	//배송 방식
